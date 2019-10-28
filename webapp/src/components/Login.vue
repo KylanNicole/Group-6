b7f4cba46dc5b0a41df3d26311a89576e13c353c
Cullen Carns <cullenjcarns@gmail.com>
2019-10-28 11:36:49 -0700

    Placeholder methods and moved things

--- webapp/src/components/Login.vue
+++ webapp/src/components/Login.vue
@@ -3,88 +3,92 @@
     <button class="button is-primary" v-on:click="modalActive = true">Login</button>
     <b-modal :active.sync="modalActive" has-modal-card>
       <div class="modal-card" style="width:auto">
-        <header class="modal-card-head">
-          <p class="modal-card-title">Login</p>
-        </header>
-        <section class="login-modal-body">
-          <section class="modal-card-body">
-            <span class="has-text-danger" v-if="error">Unsuccessful logging in.</span>
-            <b-field label="Email">
-              <b-input
-              type="email"
-              v-model="email"
-              placeholder="Your email"
-              required
-              ></b-input>
-            </b-field>
+        <!-- <header class="modal-card-head">
+        <p class="modal-card-title">Login</p>
+      </header> -->
+      <section class="login-modal-body">
+        <section class="modal-card-body">
+          <span class="has-text-danger" v-if="error">Unsuccessful logging in.</span>
+          <b-field label="Email">
+            <b-input
+            type="email"
+            v-model="email"
+            placeholder="Your email"
+            required
+            ></b-input>
+          </b-field>
 
-            <b-field label="Password">
-              <b-input
-              type="password"
-              v-model="password"
-              password-reveal
-              placeholder="Your password"
-              required
-              ></b-input>
-            </b-field>
-          </section>
-          <section class="modal-card-body">
-            <span class="has-text-danger" v-if="error">Unsuccessful logging in.</span>
-            <b-field label="Email">
-              <b-input
-              type="email"
-              v-model="email"
-              placeholder="Your email"
-              required
-              ></b-input>
-            </b-field>
-
-            <b-field label="Confirm Email">
-              <b-input
-              type="email"
-              v-model="email"
-              placeholder="Confirm Email"
-              required
-              ></b-input>
-            </b-field>
-
-            <b-field label="Password">
-              <b-input
-              type="password"
-              v-model="password"
-              password-reveal
-              placeholder="Your password"
-              required
-              ></b-input>
-            </b-field>
-
-            <b-field label="Confirm Password">
-              <b-input
-              type="password"
-              v-model="password"
-              password-reveal
-              placeholder="Confirm Password"
-              required
-              ></b-input>
-            </b-field>
-          </section>
-        </section>
-        <footer class="modal-card-foot">
-          <button class="button" type="button" v-on:click="modalActive = false">Close</button>
+          <b-field label="Password">
+            <b-input
+            type="password"
+            v-model="password"
+            password-reveal
+            placeholder="Your password"
+            required
+            ></b-input>
+          </b-field>
           <button class="button is-primary" v-on:click="login">Login</button>
-        </footer>
-      </div>
+        </section>
+        <section class="modal-card-body" style="border-left: 3px solid grey">
+          <span class="has-text-danger" v-if="error">Unsuccessful logging in.</span>
+          <b-field label="Email">
+            <b-input
+            type="email"
+            v-model="email"
+            placeholder="Your email"
+            required
+            ></b-input>
+          </b-field>
 
-    </b-modal>
-  </div>
+          <b-field label="Confirm Email">
+            <b-input
+            type="email"
+            v-model="email"
+            placeholder="Confirm Email"
+            required
+            ></b-input>
+          </b-field>
+
+          <b-field label="Password">
+            <b-input
+            type="password"
+            v-model="password"
+            password-reveal
+            placeholder="Your password"
+            required
+            ></b-input>
+          </b-field>
+
+          <b-field label="Confirm Password">
+            <b-input
+            type="password"
+            v-model="password"
+            password-reveal
+            placeholder="Confirm Password"
+            required
+            ></b-input>
+          </b-field>
+          <button class="button is-primary" v-on:click="signUp">Sign Up</button>
+        </section>
+      </section>
+      <footer class="modal-card-foot">
+        <button class="button" type="button" v-on:click="modalActive = false">Close</button>
+      </footer>
+    </div>
+
+  </b-modal>
+</div>
 </template>
 
 <script>
 export default {
   name: "Login",
   methods: {
-    openForm: function(){
-      console.log("opening");
+    login: function(){
+      console.log("log in");
+    },
+    signUp: function(){
+      console.log("sign up");
     }
   },
   data() {
@@ -102,6 +106,7 @@ export default {
 <style>
 .login-modal-body{
   float: left;
+  background: #ffffff;
 }
 .login-modal-body section{
   width: 50%;
--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -2,8 +2,6 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <Login/>
-    <p>blah blah</p>
   </div>
 </template>
 
@@ -15,8 +13,7 @@ import Login from "@/components/Login.vue";
 export default {
   name: "home",
   components: {
-    HelloWorld,
-    Login
+    HelloWorld
   }
 };
 </script>
