bff860a2c9483a40c09372e53a6d21f0bc342654
Cullen Carns <cullenjcarns@gmail.com>
2019-10-28 11:20:05 -0700

    Login Modal has fields for login and signup

--- webapp/src/components/Login.vue
+++ webapp/src/components/Login.vue
@@ -2,7 +2,79 @@
   <div>
     <button class="button is-primary" v-on:click="modalActive = true">Login</button>
     <b-modal :active.sync="modalActive" has-modal-card>
-      Hello
+      <div class="modal-card" style="width:auto">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Login</p>
+        </header>
+        <section class="login-modal-body">
+          <section class="modal-card-body">
+            <span class="has-text-danger" v-if="error">Unsuccessful logging in.</span>
+            <b-field label="Email">
+              <b-input
+              type="email"
+              v-model="email"
+              placeholder="Your email"
+              required
+              ></b-input>
+            </b-field>
+
+            <b-field label="Password">
+              <b-input
+              type="password"
+              v-model="password"
+              password-reveal
+              placeholder="Your password"
+              required
+              ></b-input>
+            </b-field>
+          </section>
+          <section class="modal-card-body">
+            <span class="has-text-danger" v-if="error">Unsuccessful logging in.</span>
+            <b-field label="Email">
+              <b-input
+              type="email"
+              v-model="email"
+              placeholder="Your email"
+              required
+              ></b-input>
+            </b-field>
+
+            <b-field label="Confirm Email">
+              <b-input
+              type="email"
+              v-model="email"
+              placeholder="Confirm Email"
+              required
+              ></b-input>
+            </b-field>
+
+            <b-field label="Password">
+              <b-input
+              type="password"
+              v-model="password"
+              password-reveal
+              placeholder="Your password"
+              required
+              ></b-input>
+            </b-field>
+
+            <b-field label="Confirm Password">
+              <b-input
+              type="password"
+              v-model="password"
+              password-reveal
+              placeholder="Confirm Password"
+              required
+              ></b-input>
+            </b-field>
+          </section>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button" type="button" v-on:click="modalActive = false">Close</button>
+          <button class="button is-primary" v-on:click="login">Login</button>
+        </footer>
+      </div>
+
     </b-modal>
   </div>
 </template>
@@ -26,3 +98,13 @@ export default {
   }
 };
 </script>
+
+<style>
+.login-modal-body{
+  float: left;
+}
+.login-modal-body section{
+  width: 50%;
+  float: left;
+}
+</style>
