0c60efaebf7e0641a71c86ce0b0b88a929f9fcfb
Cullen Carns <cullenjcarns@gmail.com>
2019-11-04 12:53:26 -0800

    b-modal is part of login component instead of header

--- webapp/src/components/Header.vue
+++ webapp/src/components/Header.vue
@@ -13,56 +13,50 @@
       <button>CART</button>
       <button>TRACK</button>
     </div>
-    <b-modal
-      :active.sync="loginModalActive"
-      has-modal-card
-    >
-      <Login/>
-    </b-modal>
-    </div>
-  </template>
+  </div>
+</template>
 
 <script>
 import SearchBar from "./SearchBar.vue";
 import Login from "./Login.vue";
 export default {
-    name: "Header",
-    components: {
-        SearchBar,
-        Login
-    }
+  name: "Header",
+  components: {
+    SearchBar,
+    Login
+  }
 };
 </script>
 
 <style scoped>
 #header {
-    width: 100%;
-    padding: 15px 15px 15px 15px;
-    border-bottom: solid #AAAAAA 1px;
-    position: sticky;
-    top: 0;
-    background-color: #FDFDFD;
+  width: 100%;
+  padding: 15px 15px 15px 15px;
+  border-bottom: solid #AAAAAA 1px;
+  position: sticky;
+  top: 0;
+  background-color: #FDFDFD;
 }
 #logo {
-    width: 20px;
-    height: 20px;
+  width: 20px;
+  height: 20px;
 }
 #user-content {
-    display: inline;
-    float: right;
+  display: inline;
+  float: right;
 }
 a {
-    text-decoration: none;
-    margin: 0 0 0 20px;
-    line-height: 30%;
+  text-decoration: none;
+  margin: 0 0 0 20px;
+  line-height: 30%;
 }
 button {
-    padding: 3px;
-    border-radius: 5px;
-    border-style: none;
-    background-color: #EEEEEE;
-    margin-left: 5px;
-    color: #888888;
-    cursor: pointer;
+  padding: 3px;
+  border-radius: 5px;
+  border-style: none;
+  background-color: #EEEEEE;
+  margin-left: 5px;
+  color: #888888;
+  cursor: pointer;
 }
 </style>
