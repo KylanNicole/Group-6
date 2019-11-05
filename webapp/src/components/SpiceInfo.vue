d695d2e0fed66078ad350025088e2667473f938a
gregdchu <gdchu@Jason.localdomain>
2019-11-04 17:07:14 -0800

    Added spice info component.

---
+++ webapp/src/components/SpiceInfo.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="spice-info">
+        <div class="desc">
+            <img src="@/assets/paprika.jpg" />
+            <h4>Paprika</h4>
+            <p>
+                A ground spice made from dried red fruits of the larger and sweeter varieties of the plant Capsicum annuum, called bell pepper or sweet pepper.
+            </p>
+        </div>
+        <div class="purchase">
+            <form>
+                <p>Amount: </p>
+                <input type="text" />
+                <select>
+                    <option default>g</option>
+                    <option>oz</option>
+                </select>
+            <br/>
+                <p>Price: $100</p>
+            <br/>
+                <p>Stock: </p>
+                <input type="text" value="100" />
+            <br/>
+                <button>Add to Cart</button>
+            <br/>
+                <p>Tags</p>
+                <p class="tag" v-for="tag in tags">{{tag}}</p>
+            </form>
+        </div>
+        <button class="edit">Edit</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SpiceInfo",
+    data() {
+        return {
+            tags: ["Red", "Spicy"]
+        }
+    }
+}
+</script>
+
+<style scoped>
+img {
+    width: 50%;
+    height: 50%;
+    margin: auto;
+}
+h4 {
+    font-weight: bold;
+}
+p {
+    font-size: 10pt;
+}
+
+.spice-info {
+    border: solid 1px black;
+    border-radius: 5px;
+    padding: 10px;
+    overflow: hidden;
+    width: 50%;
+    background-color: #F0F0F0;
+}
+.desc {
+    float: left;
+    width: 50%;
+    display:inline;
+    border-right: solid 1px black;
+    margin-right: 15px;
+}
+
+.purchase{
+    display: inline;
+}
+
+.purchase p{
+    display: inline;
+}
+.edit {
+    display:block;
+}
+.tag {
+    background-color: lightgreen;
+    border-radius: 5px;
+    padding: 0 10px 0 10px;
+    margin: 0 1px;
+}
+</style>
