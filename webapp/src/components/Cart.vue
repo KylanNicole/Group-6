6e74a53f533c19850c88428881aa739e48615e28
gregdchu <gdchu@Jason.localdomain>
2019-11-05 19:54:27 -0800

    Updated user cart.

---
+++ webapp/src/components/Cart.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="cart">
+        <div class="header">
+            <button id="exit">X</button>
+            <h1>My Cart</h1>
+        </div>
+        <CartTile/>
+        <div class="footer">
+            <router-link to="foo" style="float: left">Go To Checkout</router-link>
+            <p style="text-align: right; display: inline;">Total: $100</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import CartTile from "./CartTile.vue"
+export default {
+    name: "Cart",
+    components: { CartTile }
+}
+</script>
+
+<style scoped>
+.cart {
+    top: 0;
+    position: sticky;
+    float: right;
+    border: solid 1px black;
+    width: 300px;
+    border-radius: 5px;
+}
+h1 {
+    text-align: left;
+    margin: 0;
+}
+.header {
+    padding: 10px;
+    border-bottom: solid 1px black;
+    margin-bottom: 10px;
+}
+.footer {
+    border-top: solid 1px black;
+    padding: 10px;
+    margin-top: 10px;
+}
+#exit {
+    float: right;
+    position: relative;
+}
+</style>
---
+++ webapp/src/components/CartTile.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="carttile">
+        <button style="float: right">X</button>
+        <h3 style="margin: 0">Spice Name</h3>
+        <img style="float: left" src="@/assets/logo.png">
+        <div class="info">
+            <input style="width: 50px" type="text">
+            <select>
+                <option default>g</option>
+                <option>oz</option>
+            </select>
+            <p>Price: $10</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CartTile"
+}
+</script>
+
+<style scoped>
+.carttile {
+    background-color: #EEEEEE;
+    width: 80%;
+    margin: auto;
+    display: block;
+    padding: 5px 0 5px 0;
+    overflow: auto;
+}
+.info {
+    float: right;
+    text-align: right;
+}
+img {
+    width: 50px;
+    display: inline;
+}
+</style>
