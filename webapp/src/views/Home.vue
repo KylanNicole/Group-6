dd97cc43262525b9661c9cfbb0a8afea513736f7
gregdchu <gdchu@Jason.localdomain>
2019-11-05 20:22:55 -0800

    Removed Cart tag from Home.

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <Cart/>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
@@ -9,13 +8,11 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-import Cart from "@/components/Cart.vue"
 
 export default {
   name: "home",
   components: {
-    HelloWorld,
-    Cart
+    HelloWorld
   }
 };
 </script>
