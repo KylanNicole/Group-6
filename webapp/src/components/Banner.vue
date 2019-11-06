79db66b1c4bf138929d1320ca8317e1b61131d22
gregdchu <gdchu@Jason.localdomain>
2019-11-05 20:45:03 -0800

    Implemented /very/ basic banner.

---
+++ webapp/src/components/Banner.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="banner">
+        <h1>Banner Here</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Banner"
+}
+</script>
+
+<style scoped>
+.banner {
+    width: 100%;
+    height: 250px;
+    background-color: darkgray;
+    padding: 20px 0 20px 0;
+}
+h1 {
+    margin: 0;
+}
+</style>
