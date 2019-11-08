1797d60236c60f55048f7139bd4d259af2dea134
gregdchu <gdchu@Jason.localdomain>
2019-11-07 18:25:38 -0800

    Added Cart to Home.

--- webapp/src/views/Home.vue
+++ webapp/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="section">
+    <Cart/>
     <SpiceInfo/>
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
@@ -9,12 +10,14 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-import SpiceInfo from "@/components/SpiceInfo.vue"
+import SpiceInfo from "@/components/SpiceInfo.vue";
+import Cart from "@/components/Cart.vue"
 export default {
   name: "home",
   components: {
     HelloWorld,
-    SpiceInfo
+    SpiceInfo,
+    Cart
   }
 };
 </script>
