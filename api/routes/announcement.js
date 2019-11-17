3e7ee6d66f59f73c0afa3ced4137ffd29b50a309
Cullen Carns <cullenjcarns@gmail.com>
2019-11-17 10:00:17 -0800

    Create new announcements in database
    Fixed spelling of announcement in some places
    Updated banner creation form
    Post request for new banners

--- api/routes/announcement.js
+++ api/routes/announcement.js
@@ -8,7 +8,7 @@ import Announcement from '../entities/announcement';
 
 const router = Router();
 router.route('/announcement')
-  .all(isAuthenticated)
+  // .all(isAuthenticated)
 //   .get((req, res) => {
 //     const allItem = getRepository(Item)
 //     .createQueryBuilder("items")
@@ -21,17 +21,15 @@ router.route('/announcement')
     const { title, description, link, color } = req.body;
     const manager = getManager();
     const announce = manager.create(Announcement, { title, description, link, color });
-    
 
-    
     manager.save(announce).then((savedannounce) => {
       res.send(savedannounce);
     });
   });
 
-  
 
-  
+
+
 router.route('/announcement/:id')
   .all(isAuthenticated)
   .all((req, res, next) => {
@@ -51,12 +49,12 @@ router.route('/announcement/:id')
   .put((req, res) => {
     const foundAnnounce = req.announcement;
     const {title, description, link, color } = req.body;
-    
+
 
     foundannounce.title = title;
-    foundannounce.description = description; 
+    foundannounce.description = description;
     foundannounce.link = link;
-    foundannounce.color = color; 
+    foundannounce.color = color;
 
 
     getManager().save(foundAnnounce).then((updatedAnnounce) => {
