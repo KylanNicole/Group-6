7a1fb949f28ba368fc05a370b1ac20a23cfbf3a4
Kylan Stewart <kylan.stewart@gmail.com>
2019-11-30 11:14:59 -0800

    Items in List Form

--- webapp/src/components/Order.vue
+++ webapp/src/components/Order.vue
@@ -1,55 +1,60 @@
 <template>
-    <div class="order">
-        <h1>Order ID: {{id}}</h1>
-        <button @click="clickMethod">Details</button>
-        <div :class="{hide : hideDetails}">
-            <p>Price: ${{total_cost}}</p>
-            <p>Weight: {{total_weight}}</p>
-            <p>Items: {{item_list}}</p>
-            <p>Status: {{status}}</p>
-            <p>Customer ID: {{customer_id}}</p>
-        </div>
+  <div class="order">
+    <h1>Order ID: {{id}}</h1>
+    <button @click="clickMethod">Details</button>
+    <div :class="{hide : hideDetails}">
+      <p>Price: ${{total_cost}}</p>
+      <p>Weight: {{total_weight}}</p>
+      <p>Status: {{status}}</p>
+      <p>Customer ID: {{customer_id}}</p>
+      <p>Items:</p>
+      <div>
+        <ul v-for="i in item_list.length">
+          <li>{{item_list[i-1]}}</li>
+        </ul>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Order",
-    props: {
-        id: Number,
-        total_cost: Number,
-        total_weight: Number,
-        item_list: String,
-        status: Number,
-        customer_id: Number
-    },
-    data() {
-        return {
-            hideDetails: true
-        }
-    },
-    methods: {
-        clickMethod() {
-            this.hideDetails = !this.hideDetails;
-        }
+  name: "Order",
+  props: {
+    id: Number,
+    total_cost: Number,
+    total_weight: Number,
+    item_list: String,
+    status: Number,
+    customer_id: Number
+  },
+  data() {
+    return {
+      hideDetails: true
+    };
+  },
+  methods: {
+    clickMethod() {
+      this.hideDetails = !this.hideDetails;
     }
-}
+  }
+};
 </script>
 
 <style scoped>
-div{
-    width:1000px;
+div {
+  width: 1000px;
 }
 .order {
-    border: solid 1px black;
+  border: solid 1px black;
 }
 h1 {
-    display: inline;
+  display: inline;
 }
 button {
-    float: right;
+  float: right;
 }
 .hide {
-    display: none;
+  display: none;
 }
 </style>
